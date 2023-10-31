3c5694694cd543f54131d377606eebc8a48df85c
vakharwalad23 <vakharwala.d@gmail.com>
2023-11-01 02:48:10 +0530

    feat: added grpc user auth demo

---
+++ apps/gateway/src/users/users.service.ts
@@ -0,0 +1,60 @@
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  CreateUserDto,
+  PaginationDto,
+  USER_SERVICE_NAME,
+  UpdateUserDto,
+  UserServiceClient,
+} from '@dav/common';
+import { AUTH_SERVICE } from './constants';
+import { ClientGrpc } from '@nestjs/microservices';
+import { ReplaySubject } from 'rxjs';
+
+@Injectable()
+export class UsersService implements OnModuleInit {
+  private usersService: UserServiceClient;
+
+  constructor(@Inject(AUTH_SERVICE) private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.usersService =
+      this.client.getService<UserServiceClient>(USER_SERVICE_NAME);
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  findAll() {
+    return this.usersService.findAllUsers({});
+  }
+
+  findOne(id: string) {
+    return this.usersService.findOneUser({ id });
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.usersService.updateUser({ id, ...updateUserDto });
+  }
+
+  remove(id: string) {
+    return this.usersService.removeUser({ id });
+  }
+
+  emailUsers() {
+    const users$ = new ReplaySubject<PaginationDto>();
+    users$.next({ page: 0, skip: 25 });
+    users$.next({ page: 1, skip: 25 });
+    users$.next({ page: 2, skip: 25 });
+    users$.next({ page: 3, skip: 25 });
+
+    users$.complete();
+    let chunk = 1;
+
+    this.usersService.queryUsers(users$).subscribe((users) => {
+      console.log('Chunk:', chunk);
+      console.log(users);
+      chunk++;
+    });
+  }
+}
